9ec3a17a422513f3e50a1bd69999bfd3825a4a8b
piyaladamas2 <piyal.adamas2@gmail.com>
2025-09-18 20:01:39 +0530

    initial set up completed

---
+++ src/main/java/com/piyal/enveuproject/EnveuprojectApplication.java
@@ -0,0 +1,13 @@
+package com.piyal.enveuproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnveuprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnveuprojectApplication.class, args);
+	}
+
+}
